f8312b833e491936d9ac321e813da93b77a1fec9
Manas93 <manas.chhabra93@gmail.com>
2019-11-28 14:14:31 +0530

    Update variable.tf

--- vm-with-ip/variable.tf
+++ vm-with-ip/variable.tf
@@ -1,3 +1,44 @@
+variable "datastore" {
+ type = "string"
+}
+
+variable "vmname" {
+ type = "string"
+}
+variable "image" {
+  type = "string"
+}
+variable "network_name" {
+  type = "string"
+}
+variable "diskname" {
+  type = "string"
+}
+variable "hostname" {
+ type = "string"
+ default = "vm-two"
+}
+variable "resourcepool" {
+  type = "string"
+}
+variable "ipv4_address" {
+  type = "string"
+  default = "10.198.4.157"
+}
+variable "ipv4_netmask" {
+  type = "string"
+  default = 24
+}
+variable "ipv4_gateway" {
+  type = "string"
+  default = "10.198.4.1"
+}
+variable "dns_server_list" {
+  type = "list"
+  default = ["10.198.4.10"]
+}
+
+
 variable "user" {
     type = "string"
 }
@@ -10,3 +51,4 @@ variable "host" {
 variable "region" {
     type = "string"
 }
+
